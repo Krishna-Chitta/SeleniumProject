81fa943af88667e6442679dfd30f063a87b0f2c6
M0NS13UR-K <krishnach073@gmail.com>
2025-03-14 21:53:20 -0700

    update dropdown operations

--- Dropdowns.py
+++ Dropdowns.py
@@ -6,11 +6,24 @@ from selenium.webdriver.support.select import Select
 
 driver = webdriver.Chrome()
 driver.maximize_window()
-driver.get("https://the-internet.herokuapp.com/")
+login_url = "https://the-internet.herokuapp.com/"
+driver.get(login_url)
 driver.find_element(By.XPATH,"//a[normalize-space()='Dropdown']").click()
 dropdown_element = driver.find_element(By.ID,"dropdown")
-select = Select(dropdown_element)
+select = Select(dropdown_element) #Select class is used to perform operations on dropdowns.
+#count of total dropdowns could be obtained by using select.options
+options_count = len(select.options)
+# 3 different ways to perform actions on dropdowns
+#select by visible_text:
 select.select_by_visible_text("Option 2")
 time.sleep(1)
+#select by index:
+select.select_by_index(1) #should select 'option 1'
+time.sleep(1)
+#select by option using value:
+select.select_by_value("2") #should select 'option 2'
+time.sleep(1)
+
+print(options_count) #count would be 3 because it will also consider "please select an option" as an option
 
 driver.close()
