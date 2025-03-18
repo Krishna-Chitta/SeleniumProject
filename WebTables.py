a7ef3304d65cfa382aaf2bdf6c4b64a0b8963e5c
M0NS13UR-K <krishnach073@gmail.com>
2025-03-17 22:27:15 -0700

    append webtables

--- WebTables.py
+++ WebTables.py
@@ -1,8 +1,12 @@
-from selenium import webdriver
-from selenium.webdriver.common import by
+import time
 
-url = "www.sf.com"
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+url = "https://the-internet.herokuapp.com/"
 driver = webdriver.Chrome()
 driver.get(url)
+driver.find_element(By.XPATH, "//a[normalize-space()='Sortable Data Tables']").click()
 
+time.sleep(2)
 driver.close()
